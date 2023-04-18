9f6ff27fed51a38765d86a3f3fdcc4554a16b1fc
Alena Yarmak <alena.yarmak@protonmail.com>
2023-04-18 16:26:30 +0200

    persist list when we reodred tasks inside one column

--- src/App.js
+++ src/App.js
@@ -11,7 +11,38 @@ class App extends Component {
   state = InitialState;
 
   onDragEnd = (result) => {
-    /*  */
+    const {source, destination, draggableId} = result;
+
+    if (!destination) {
+      return;
+    }
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+    if (destination.droppableId === source.droppableId) {
+      const column = this.state.columns[destination.droppableId];
+      const updTaskIds = this.state.columns[destination.droppableId].taskIds;
+      /* delete task from array */
+      updTaskIds.splice(source.index, 1);
+      /* add task draggableId to new place */
+      updTaskIds.splice(destination.index, 0, draggableId);
+
+      const updColumn = {
+        ...column,
+        taskIds: updTaskIds
+      };
+      console.log(updColumn);
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [updColumn.id]: updColumn
+        }
+      }
+      console.log(newState);
+
+      this.setState(newState);
+    }
   }
 
   render() {
--- src/InitialState.js
+++ src/InitialState.js
@@ -4,6 +4,8 @@ const InitialState = {
         'task-2': {id: 'task-2', content: 'Clean kitchen'},
         'task-3': {id: 'task-3', content: 'Do dnd Aplication'},
         'task-4': {id: 'task-4', content: 'Watch "Blade Runner"'},
+        'task-5': {id: 'task-5', content: 'Aplication'},
+        'task-6': {id: 'task-6', content: '"Blade Runner"'},
     },
     /* for store columns */
     /* column id I use like key */
@@ -14,14 +16,14 @@ const InitialState = {
             title: 'To do',
             taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
         },
-        /* 'column-2': {
+        'column-2': {
             id: 'column-2',
             title: 'In Progress',
-            taskIDs: ['task-3', 'task-4']
-        } */
+            taskIds: ['task-5', 'task-6']
+        }
     },
     /* order columns */
-    columnOrder: ['column-1'/* , 'column-2' */]
+    columnOrder: ['column-1', 'column-2']
 }
 
 export default InitialState;
