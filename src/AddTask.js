e4e476b41989b171506333e5830b5bfad84d07a2
Alena Yarmak <alena.yarmak@protonmail.com>
2023-04-23 19:56:48 +0200

    add makeId function / fix problem with state

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,32 +1,115 @@
 import React from "react";
 import { Component } from "react";
+import InitialState from './InitialState';
+
+import { useEffect } from "react";
 
 class AddTask extends Component {
 
-    constructor(props) {
+    /* constructor(props) {
         super(props);
         this.state = {
-          task: "",
+          newTask: '',
         };
-      }
+      } */
+      state = InitialState;
+
+      /* handleNewTaskChange = (event) => {
+        this.setState({ newTask: event.target.value });
+        this.setState({ newTask: 'Test passed'})
+      }; */
 
       handleAddTask = (e) => {
-        console.log(1);
-        console.log(this.state.task);
+        /* let newTaskId = Date.now(); */
+        /* let newTaskId = "task-" + (Object.keys(InitialState.tasks).length + 1);
+        const newTaskContent = this.state.newTask; */
+
+        const taskId = 'task-9';
+        const newTask = 'Test passed';
+
+        const task = {id: 'task-9', content: newTask};
+        const newTaskIds = ['task-1', 'task-2', 'task-3', 'task-4', 'task-9'];
+
+        const newState = {
+          tasks: {
+            ...InitialState.tasks,
+            [taskId]: task
+          },
+          columns: {
+            ...InitialState.columns,
+            'column-1': {
+              id: 'column-1',
+              title: 'To do',
+              taskIds: newTaskIds
+            }
+          },
+          columnOrder: [...InitialState.columnOrder]
+        }
+
+        console.log(newState);
+
+        /* if (InitialState !== newState) {
+          this.setState(newState);
+        } */
+        /* this.setState(newState); */
+        /* componentDidUpdate(InitialState) {
+          if (InitialState !== newState) {
+            this.setState(newState);
+          }
+        }
+        console.log(InitialState); */
+
+        useEffect(() => {
+          if (InitialState !== newState) {
+            this.setState(newState);
+          }
+        })
+
+        /* let updColumnTasks = InitialState.columns["column-1"].taskIds;
+        let updTaskIds = updColumnTasks.push(newTaskId);
+
+        console.log(updColumnTasks);
+        console.log(updTaskIds);
+        console.log(InitialState); */
+        /* let newTasks = InitialState.tasks;
+        newTasks[newTaskId] = {id: newTaskId, content: newTaskContent}
+        console.log(InitialState); */
+
+        /* let updState = {
+            tasks: newTasks,
+            columns: {
+                ...InitialState.columns,
+                'column-1': {
+                    id: 'column-2',
+                    title: 'In Progress',
+                    taskIds: updColumnTasks}
+
+            },
+            columnOrder: [...InitialState.columnOrder]
+        } */
+
+        /* if (InitialState !== updState) {
+          this.setState(updState);
+        } */
+
+        /* console.log(InitialState); */
+        /* console.log(updState); */
+        
+
       }
 
     render() {
         return (
-            <div class="input-group mb-3 w-25 mt-3">
+            <div className="input-group mb-3 w-25 mt-3">
                 <input 
                 type="text" 
-                class="form-control" 
+                className="form-control" 
                 placeholder="Enter your task" 
                 aria-label="Enter your task" 
                 aria-describedby="button-addon2"
-                onChange={(e) => this.setState({ task: e.target.value })}></input>
+                onChange={this.handleNewTaskChange}></input>
                 <button 
-                class="btn btn-light btn-outline-dark" 
+                className="btn btn-light btn-outline-dark" 
                 type="button" 
                 id="button-addon2"
                 onClick={this.handleAddTask}>Add Task</button>
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Component } from 'react';
 import { DragDropContext} from 'react-beautiful-dnd';
 import InitialState from './InitialState';
 import Column from './Column';
-import AddTask from './AddTask';
+/* import AddTask from './AddTask'; */
 
 import './App.css';
 
@@ -12,6 +12,16 @@ import './App.css';
 
 class App extends Component {
   state = InitialState;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Override the 'newTask' property from the initial state
+      newTask: '',
+      // Keep the other properties from the initial state
+      ...this.state,
+    };
+  }
 
   onDragEnd = (result) => {
     const {source, destination, draggableId} = result;
@@ -25,7 +35,6 @@ class App extends Component {
     if (destination.droppableId === source.droppableId) {
       const column = this.state.columns[destination.droppableId];
       const updTaskIds = column.taskIds;
-
       /* delete task from array */
       updTaskIds.splice(source.index, 1);
       /* add task draggableId to new place */
@@ -78,6 +87,63 @@ class App extends Component {
     }
   }
 
+  handleNewTaskChange = (e) => {
+    this.setState({ newTask: e.target.value });
+  }
+
+  makeId = () => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < 8) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+  handleAddTask = (e) => {
+    const taskId = this.makeId();
+
+        const newTask = this.state.newTask;
+
+        const task = {id: taskId, content: newTask};
+        const newTaskIds = this.state.columns['column-1'].taskIds;
+        newTaskIds.push(taskId);
+
+        const test = {tasks: {
+          ...this.state.tasks,
+          taskId: task
+        }}
+        console.log(test);
+
+        const newState = {
+          tasks: {
+            ...this.state.tasks ,
+            [taskId]: task
+          },
+          columns: {
+            ...this.state.columns,
+            'column-1': {
+              id: 'column-1',
+              title: 'To do',
+              taskIds: newTaskIds
+            }
+          },
+          columnOrder: [...this.state.columnOrder]
+        }
+
+
+        /* this.setState(newState); */
+        
+        this.setState(newState, () => {
+          this.setState({ newTask: '' });
+        });
+        console.log(this.state);
+      }
+
+
   render() {
     return (
       /* our drag and drop area */
@@ -97,7 +163,21 @@ class App extends Component {
       })}
         </div>
       </DragDropContext>
-        <AddTask />
+      <div className="input-group mb-3 w-25 mt-3">
+                <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Enter your task" 
+                aria-label="Enter your task" 
+                aria-describedby="button-addon2"
+                value={this.state.newTask}
+                onChange={this.handleNewTaskChange}></input>
+                <button 
+                className="btn btn-light btn-outline-dark" 
+                type="button" 
+                id="button-addon2"
+                onClick={this.handleAddTask}>Add Task</button>
+            </div>
         </div>
       </div>
       
