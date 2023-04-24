97f1077d311a31c7511ad0771868c9b24b3dd272
Alena Yarmak <alena.yarmak@protonmail.com>
2023-04-24 19:46:05 +0200

    add delete button / add styles for delete button

--- src/Task.js
+++ src/Task.js
@@ -4,16 +4,27 @@ import { Draggable } from "react-beautiful-dnd";
 
 class Task extends Component {
     render() {
+        const handleDelete = (props) => {
+            console.log(this.props.index, this.props.task.id);
+          }
         return(
             <Draggable
                 draggableId={this.props.task.id}
-                index={this.props.index}>
+                index={this.props.index}
+                >
                     {provided => (
-                        <div
+
+                            <div
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
                             ref={provided.innerRef}
-                            className='bg-white mb-2 p-2 rounded'>{this.props.task.content}</div>
+                            className='bg-white mb-2 p-2 rounded d-flex justify-content-between align-items-center'>{this.props.task.content}
+                            <button onClick={handleDelete}
+                            className='btn btn-danger m-2'
+                            >Delete</button>
+                            </div>
+
+                     
                     )}
             </Draggable>
         )
